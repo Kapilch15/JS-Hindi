167fc9d675193cb85230e2c91f62ee7a5c1203e4
Kapil Chaudhary <115942044+Kapilch15@users.noreply.github.com>
2023-08-03 07:19:38 +0000

    my commit

---
+++ 01_Basics/dataTypes.js
@@ -0,0 +1,30 @@
+//DataTypes in JS
+          
+let name = "Kapil"      //String
+const age = 19          //Number
+let isLoggedIn = false  //Boolean
+
+//Number --> 2 to power 53
+//String
+//Bigint
+//Boolean --> true/false
+//null  --> value that represent empty or unknwn value/ Object 
+//undefined --> a variable that has no value assign
+//Symbol
+//Object
+
+
+
+//CONVERSION of DataTypes 
+let score = 344;
+let valInNumber = Number("34abc");
+
+console.log(typeof score);
+console.log(typeof valInNumber);
+
+console.log(valInNumber);
+
+//344 => 34
+//"344abc" => NaN - Not a Number
+//true => 1 false - 0
+ 
