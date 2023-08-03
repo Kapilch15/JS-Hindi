e98beb36955acf0efbf507ee218f81465a24901d
Kapil Chaudhary <115942044+Kapilch15@users.noreply.github.com>
2023-08-03 06:39:31 +0000

    let var const difference

--- 01_Basics/test.js
+++ 01_Basics/test.js
@@ -1,1 +1,19 @@
 console.log("Interview Prepration");
+
+const accountId = 22334;
+let accountName = "Ankit Chaudhary";
+var accountPassword = "542231"
+accountCity = "Agra";
+
+/*
+Prefer not to use var
+becoz issue in block scope & functional scope
+*/
+
+// accountId = 2345;
+accountName = "Ashish";
+accountCity = "Mumbai";
+accountName = "1234";
+
+console.log(accountId);
+console.table([accountId, accountName, accountCity, accountPassword]);
