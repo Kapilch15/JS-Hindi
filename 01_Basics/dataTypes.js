c53b28485bc68ec2f63da3eb0b5c607981dd317a
Kapil Chaudhary <115942044+Kapilch15@users.noreply.github.com>
2023-08-05 06:19:44 +0000

    datatTypes done

--- 01_Basics/dataTypes.js
+++ 01_Basics/dataTypes.js
@@ -16,15 +16,61 @@ let isLoggedIn = false  //Boolean
 
 
 //CONVERSION of DataTypes 
-let score = 344;
-let valInNumber = Number("34abc");
+// let score = 344;
+// let valInNumber = Number("34abc");
 
-console.log(typeof score);
-console.log(typeof valInNumber);
+// console.log(typeof score);
+// console.log(typeof valInNumber);
 
-console.log(valInNumber);
+// console.log(valInNumber);
 
 //344 => 34
 //"344abc" => NaN - Not a Number
 //true => 1 false - 0
  
+
+// ****************************OPERATIONS **************************
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(6-2);
+// console.log(12*4);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(4%2);
+
+let str1 = "hello ";
+let str2 = "Kapil";
+let str3 = str1 + str2;
+
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "7");
+
+// //Comparisions oprator
+// console.log(3>2);
+// console.log((2 >= 3));
+// console.log(2 < 3);
+// console.log(3 != 4);
+
+// ********SUMMARY******************
+// Primitive dataTpyes
+
+// 7Types :- String, Number, Booloean, null, undefined, symbol, BigInt
+
+//Reference (Non-primitive)
+
+// Array, Objects, functions
+
+//Javascript is Dynamically typed language but Typescript is Static, In Dynamic language
+//  all the type checks in the runtime.
+//   let a = 12;
+//   console.log(a);
+
+// ********************************************************************
+
---
+++ 01_Basics/heapStack.js
@@ -0,0 +1,20 @@
+// ***************HEAP and Stack *****************
+
+let myYoutubeName = "KapilChaudhary";
+
+let anotherName = myYoutubeName;
+anotherName = "chai and Code";
+
+// console.log(myYoutubeName);
+// console.log(anotherName);
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl",
+}
+
+let userTwo = userOne;
+userTwo.email = "kk234@google.com";
+
+// console.log(userOne.email);
+// console.log(userTwo.email); 
