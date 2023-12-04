0f1ac6603802451344fadee68b1ed17825f9f58c
Kapil Chaudhary <115942044+Kapilch15@users.noreply.github.com>
2023-12-04 09:50:10 +0000

    update arrow.js

--- JS/arrow.js
+++ JS/arrow.js
@@ -1,35 +1,42 @@
 const user = {
-    username: "Ankit",
-    price: 899,
-    course: "Web development",
-    welcomeMessage: function (){
-        console.log(`${this.username} ,Welcome to the course`);
+    username: "Kapil",
+    price: 599,
+    book:  "WEB 3.0",
+    welcomeMsg: function () {
+        console.log(`${this.username} , welcome to Library`);
         console.log(this);
     }
 }
 
-// user.welcomeMessage();
-// user.username = "Manish";
-// user.welcomeMessage();
+// user.welcomeMsg();
+// user.username = "Sameer";
+// user.welcomeMsg();
 
 // console.log(this);
 
-/************************arrow function ******** */
-const register = () =>{
-    let name = "Manu";
-    console.log(this.name);
-}
+// function chaiCode () {
+//     let username = "Kapil"
+//     console.log(this.username);      //Here we cannot access variable with 'this' keyword
+// }
 
-// register();
+// chaiCode();
 
-const addTwo =(num1, num2) =>{
-   return num1 + num2;
-}
-console.log(addTwo(45, 50));
+//FUNCTION DECLARATION : (arrow function)
+// const chaiCode = () =>{
+//     let username = "Kapil";
+//     console.log(this.username);
+// }
+// chaiCode();
 
-addTwo();
+//  const addTwo = (num1, num2) => {
+//     return num1 + num2;
+//  }
+//  console.log(addTwo(13,7));
 
-//implicit Arrow function
-const add =(num1, num2) => num1 + num2;
- console.log(addTwo(45, 50));
- 
+ //Implicit function
+//  const addTwo = (num1, num2) => num1 + num2;
+//  console.log(addTwo(34, 409));
+
+
+const addTwo = (num1, num2) =>( { username: "UserName"})
+console.log(addTwo(20, 20));
